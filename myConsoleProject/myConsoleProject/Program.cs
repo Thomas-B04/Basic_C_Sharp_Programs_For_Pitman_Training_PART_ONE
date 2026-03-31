754a672681045fa54c531c0bd5839f7b188cff67
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-03-31 23:04:22 +0100

    Add welcome messages to Sample_Application Main method

--- myConsoleProject/myConsoleProject/Program.cs
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -6,5 +6,7 @@ class Sample_Application
     static void Main()
     {
         Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome to C# programming.");
+        Console.WriteLine("This is a simple console application.");
     }
 }
