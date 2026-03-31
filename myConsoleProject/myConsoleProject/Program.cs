9bcddfe7fe6b314d12acfadaf274d1c4f0e01e7b
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-03-31 23:07:54 +0100

    Add Sample_Application class with Main method

--- myConsoleProject/myConsoleProject/Program.cs
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-class Sample_Application
-
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Welcome to C# programming.");
-        Console.WriteLine("This is a simple console application.");
-    }
-}
+﻿
