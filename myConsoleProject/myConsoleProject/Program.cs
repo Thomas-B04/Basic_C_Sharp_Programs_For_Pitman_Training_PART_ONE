6eaecb12ad0df2afc5fed9a0fd831cab75f37f1f
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-03-31 23:20:53 +0100

    Move Sample_Application class to Program.cs
    Relocated the entire Sample_Application class and its Main method from Sample_Application.cs to Program.cs. Removed Sample_Application.cs as it is no longer needed. No changes were made to the application's functionality.

--- myConsoleProject/myConsoleProject/Program.cs
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+class Sample_Application
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome to C# programming.");
+        Console.WriteLine("This is a simple console application.");
+    }
+}
