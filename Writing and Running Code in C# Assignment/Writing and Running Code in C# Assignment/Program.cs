5ee3faf0b8c5924c4ef8069a66561c6625378b72
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-04-06 16:56:16 +0100

    Add initial C# project with greeting console app
    Initialized solution and project files targeting .NET 10.0. Added Program.cs with a Greeting class that prints a welcome message and pun to the console.

---
+++ Writing and Running Code in C# Assignment/Writing and Running Code in C# Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Accounting_Greeting
+{
+    class Greeting
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Welcome to Acme Accounting Systems! \n");
+            Console.WriteLine($"Remember, we're \"accounting\" on you! \n");
+        }
+    }
+}
