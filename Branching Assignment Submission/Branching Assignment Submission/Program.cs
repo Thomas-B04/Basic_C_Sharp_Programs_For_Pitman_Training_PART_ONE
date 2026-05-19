3a4f37ea08a00dbdcf695dccd6c0f958d2b8ec2f
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-05-19 12:29:38 +0100

    Add package shipping logic and project setup
    Added a solution file referencing the project file. Created a new project targeting .NET 10.0 with necessary properties. Implemented `Program.cs` to handle package shipping logic, including user input, validation for weight and dimensions, and shipping quote calculation.

---
+++ Branching Assignment Submission/Branching Assignment Submission/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Package_express
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+
+            Console.WriteLine("Please enter the package weight: ");
+            decimal weight = Convert.ToDecimal(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the package width: ");
+            decimal width = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height: ");
+            decimal height = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length: ");
+            decimal length = Convert.ToDecimal(Console.ReadLine());
+
+            decimal dimension_total = width + height + length;
+
+            if (dimension_total > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            decimal quote = (width * height * length * weight) / 100;
+            Console.WriteLine($"Your estimated total for shipping this package is: ${quote.ToString("F2")}");
+
+            Console.WriteLine("Thank you for using Package Express. Have a good day!");
+        }
+    }
+}
